0f676a52d2b00b31a798b93682970ef4462bc270
Seneth Jayashan <info@sjaywebsolutions.lk>
2025-01-22 01:13:58 +0530

    Delete src directory

--- src/Admin/pages/managers.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function managers() {
-  return (
-    <div>managers</div>
-  )
-}
--- src/Admin/pages/promotions.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function promotions() {
-  return (
-    <div>promotions</div>
-  )
-}
